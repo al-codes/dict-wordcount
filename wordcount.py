96fc2783f3b19f6ea3fab195bb34216c4f02232c
Alexandra Sanchez <alexlsanchez@gmail.com>
2020-07-13 19:34:45 +0000

    Completed function

--- wordcount.py
+++ wordcount.py
@@ -1,15 +1,26 @@
-# opens a file and counts how many times each space-separated word occurs
-# in that file. Your program should then print those counts to the screen
-#counts words and returns counts
-#add words to dictionary by looping througn and adding count by changing 
-#dictionary values
-
-
 def get_word_count(filename):
-    """Reads file and returns count of each word"""
-    text = open(filename)
+    """Reads file and returns count of each word in a file"""
+    
+    #opens file
+    text_file = open(filename)
 
-    for line in text:
-        print(line)
+    #empty list for words and counts
+    words_counts_list = {}
+
+    #loops though lines and separates words into a list
+    for line in text_file:
+        line = line.strip()
+        words = line.split()
+        
+        #takes out punctuation marks and adds words to word count
+        for word in words:
+            word = word.strip(",.?")
+            word = word.title()
+            words_counts_list[word] = words_counts_list.get(word, 0) + 1
+
+    #prints out counts of each word in file 
+    for word, count in words_counts_list.items():
+        print(word, count)
+        
 
 get_word_count("test.txt")
